60e7eb1343bb4804f6ad460be81bdf935a396f1d
ChenHaolong <1036749218@qq.com>
2023-10-22 16:38:24 +0800

    1402.做菜顺序

---
+++ src/zzz_everyday/MaxSatisfaction.java
@@ -0,0 +1,19 @@
+package zzz_everyday;
+
+import java.util.Arrays;
+
+public class MaxSatisfaction {
+    public int maxSatisfaction(int[] satisfaction) {
+        Arrays.sort(satisfaction);
+
+        int ans = 0, s = 0;
+        for (int i = satisfaction.length - 1; i >= 0; --i) {
+            s += satisfaction[i];
+            if (s <= 0) {
+                break;
+            }
+            ans += s;
+        }
+        return ans;
+    }
+}
