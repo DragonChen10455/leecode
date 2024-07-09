607a86e82f0d48e496bd9e035a5151dcb9a75840
ChenHaolong <1036749218@qq.com>
2024-07-09 17:26:20 +0800

    3102. 最小化曼哈顿距离

---
+++ src/zzz_everyday/MinimumDistance.java
@@ -0,0 +1,32 @@
+package zzz_everyday;
+
+import java.util.TreeMap;
+
+public class MinimumDistance {
+    public int minimumDistance(int[][] points) {
+        TreeMap<Integer, Integer> xs = new TreeMap<>();
+        TreeMap<Integer, Integer> ys = new TreeMap<>();
+        for(int[] p : points){
+            xs.merge(p[0] + p[1], 1, Integer::sum);
+            ys.merge(p[1] - p[0], 1, Integer::sum);
+        }
+
+        int ans = Integer.MAX_VALUE;
+        for(int[] p : points){
+            int x = p[0] + p[1];
+            int y = p[1] - p[0];
+            if(xs.get(x) == 1) xs.remove(x);
+            else xs.merge(x, -1, Integer::sum);
+            if(ys.get(y) == 1) ys.remove(y);
+            else ys.merge(y, -1, Integer::sum);
+
+            int dx = xs.lastKey() - xs.firstKey();
+            int dy = ys.lastKey() - ys.firstKey();
+            ans = Math.min(ans, Math.max(dx, dy));
+
+            xs.merge(x, 1, Integer::sum);
+            ys.merge(y, 1, Integer::sum);
+        }
+        return ans;
+    }
+}
